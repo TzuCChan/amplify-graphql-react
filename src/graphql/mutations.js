68c65d94781911cfe8b9d7c942016548f39bf959
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-04-12 14:54:00 -0400

    mutation.js

---
+++ src/graphql/mutations.js
@@ -0,0 +1,15 @@
+export const createNote = /* GraphQL */ `
+  mutation CreateNote(
+    $input: CreateNoteInput!
+    $condition: ModelNoteConditionInput
+  ) {
+    createNote(input: $input, condition: $condition) {
+      id
+      name
+      description
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
