bb7ae5c1f3318abeae7060a41898689310839163
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-04-17 16:03:12 -0400

    app.js import export

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import { API, Storage } from "aws-amplify";
+
+export default withAuthenticator(App);
