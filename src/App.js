ef89c93649a26bd7dfb70e509cbd24b7a2b5bfa0
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-04-17 16:36:28 -0400

    app.js imports

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import { API, Storage } from "aws-amplify";
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Text,
+  TextField,
+  View,
+  withAuthenticator,
+} from "@aws-amplify/ui-react";
+import { listNotes } from "./graphql/queries";
+import {
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
+} from "./graphql/mutations";
 
 export default withAuthenticator(App);
